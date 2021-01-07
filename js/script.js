3995be47f5750077c89b461f964ae6679c61a3bd
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-01-07 20:26:48 -0300

    accordion transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import ScrollSuave from './modules/scroll-suave.js';
 
-import initAccordion from './modules/accordion.js';
+import Accordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
@@ -14,8 +14,9 @@ import initAnimacaoScroll from './modules/scroll-animacao.js';
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
 
-initAccordion();
 initTabNav();
 initModal();
 initTooltip();
